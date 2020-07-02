4d154aac0c6c2a9b5d017ab80ba6638cf68285ca
malay10 <malaybhakta10@gmail.com>
2020-07-02 11:47:25 -0500

    Updates

---
+++ precache-manifest.ff5e5a50f6e711e1a635e2f9cec042c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee0eb684745fbd631df55ee2124b526",
+    "url": "/SimpleTodoApp/index.html"
+  },
+  {
+    "revision": "d8369fc937cddf90fe78",
+    "url": "/SimpleTodoApp/static/css/main.1ee05f12.chunk.css"
+  },
+  {
+    "revision": "cb23a066fe5a3ceecc76",
+    "url": "/SimpleTodoApp/static/js/2.2d9229e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SimpleTodoApp/static/js/2.2d9229e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8369fc937cddf90fe78",
+    "url": "/SimpleTodoApp/static/js/main.1b802eed.chunk.js"
+  },
+  {
+    "revision": "de4affd0a826b0a486e9",
+    "url": "/SimpleTodoApp/static/js/runtime-main.ff0d5676.js"
+  }
+]);
